bd445cb9048d1b80c72dfd781e9e50551fad038d
joaov <joaovitork135@gmail.com>
2021-10-29 11:53:19 -0300

    a

--- src/main/java/br/com/example/export_data/web/controller/UserController.java
+++ src/main/java/br/com/example/export_data/web/controller/UserController.java
@@ -4,10 +4,16 @@ import br.com.example.export_data.domain.User;
 import br.com.example.export_data.web.pdf.UserPDFExporter;
 import br.com.example.export_data.service.UserService;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.pdf.PdfDocument;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,7 +29,7 @@ public class UserController {
     private UserService service;
 
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
         return "index";
     }
 
@@ -34,10 +40,21 @@ public class UserController {
         return "redirect:/";
     }
 
+    @RequestMapping(value = "/load", produces = MediaType.ALL_VALUE)
+    @ResponseBody
+    public void getImage(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        List<User> listUsers = service.listAll();
+
+        UserPDFExporter exporter = new UserPDFExporter(listUsers);
+
+        exporter.export(response);
+    }
+
     @GetMapping("/users/export/pdf")
     public void exportToPDF(HttpServletResponse response) throws DocumentException, IOException {
         response.setContentType("application/pdf");
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
+        DateFormat dateFormatter = new SimpleDateFormat("dd-MM-yyyy_HH:mm:ss");
         String currentDateTime = dateFormatter.format(new Date());
 
         String headerKey = "Content-Disposition";
